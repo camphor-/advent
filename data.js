be8c04127aebe6a3fa6e372b1e511cbba2874e93
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2014-11-30 21:40:51 +0900

    Update schedule

---
+++ data.js
@@ -0,0 +1,181 @@
+var schedule = [
+{
+  date: "12/1",
+  author: "ymyzk",
+  author_url: "http://www.ymyzk.com",
+  entry_title: "CAMPHOR- アドベントカレンダー 2014 を始めます!",
+  entry_url: "http://blog.camph.net/news/advent-calendar-2014/"
+},
+{
+  date: "12/2",
+  author: "ohmuraken",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+},
+{
+  date: "12/3",
+  author: "morishin",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+},
+{
+  date: "12/4",
+  author: "kohey18",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+},
+{
+  date: "12/5",
+  author: "Shtr28",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+},
+{
+  date: "12/6",
+  author: "yaitaimo",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+},
+{
+  date: "12/7",
+  author: "準備中",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+},
+{
+  date: "12/8",
+  author: "asadaman",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+},
+{
+  date: "12/9",
+  author: "akaaariiiiin",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+},
+{
+  date: "12/10",
+  author: "To-Mikasa",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+},
+{
+  date: "12/11",
+  author: "ryota-ka",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+},
+{
+  date: "12/12",
+  author: "siriusjack",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+},
+{
+  date: "12/13",
+  author: "ivstivs",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+},
+{
+  date: "12/14",
+  author: "kakenn",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+},
+{
+  date: "12/15",
+  author: "準備中",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+},
+{
+  date: "12/16",
+  author: "準備中",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+},
+{
+  date: "12/17",
+  author: "準備中",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+},
+{
+  date: "12/18",
+  author: "準備中",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+},
+{
+  date: "12/19",
+  author: "準備中",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+},
+{
+  date: "12/20",
+  author: "準備中",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+},
+{
+  date: "12/21",
+  author: "準備中",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+}
+,
+{
+  date: "12/22",
+  author: "準備中",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+}
+,
+{
+  date: "12/23",
+  author: "準備中",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+}
+,
+{
+  date: "12/24",
+  author: "準備中",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+}
+,
+{
+  date: "12/25",
+  author: "準備中",
+  author_url: "#",
+  entry_title: "準備中",
+  entry_url: "#"
+}
+];
