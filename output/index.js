91b3efd54f38a9a80923727cb9a36c8304d1655a
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2017-12-22 22:53:00 +0900

    Improve Hateb API usage

--- output/index.js
+++ output/index.js
@@ -1,20 +1,35 @@
 $(document).ready(function () {
   "use strict";
-  var $table = $('table.schedule');
-  // Get Hatena Bookmark count
-  $(".hateb-link").each(function (i, item) {
-    var $item = $(item);
-    var url = $item.data("url");
-    if (url === "#" || url === "") { return }
-    $.ajax({
-      url: "https://b.hatena.ne.jp/entry.count",
-      data: {
-        url: encodeURI(url)
-      },
-      dataType: "jsonp"
-    }).done(function (count) {
-      var suffix = count === 1 ? "User" : "Users";
-      $item.html(count + " " + suffix);
+  var $table = $("table.schedule");
+
+  var urlTo$ = {};
+  $(".hateb-link")
+    .each(function (i, item) {
+      var $item = $(item);
+      var url = $item.data("url");
+      if (url === "#" || url === "") { return; }
+      urlTo$[url] = $item;
     });
-  });
+
+  var urls = Object.keys(urlTo$);
+  var PER_PAGE = 50;
+
+  for (var page = 0; page < Math.ceil(urls.length / PER_PAGE); page++) {
+    var query = urls
+      .slice(page * PER_PAGE, (page + 1) * PER_PAGE)
+      .map(function(link) { return "url=" + encodeURIComponent(link); })
+      .join("&");
+    $.ajax({
+      url: "https://b.hatena.ne.jp/entry.counts?" + query,
+      dataType: "jsonp"
+    }).done(function (counts) {
+      Object.entries(counts).forEach(function(urlCount) {
+        var url = urlCount[0];
+        var count = urlCount[1];
+        var $item = urlTo$[url];
+        var suffix = count === 1 ? "User" : "Users";
+        $item.html(count + " " + suffix);
+      });
+    });
+  }
 });
