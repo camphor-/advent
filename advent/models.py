e1b7e99f6a14a5009342c1ca8f2b24c34cfeab4d
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2018-12-03 00:03:20 +0900

    Use dataclass in models

--- advent/models.py
+++ advent/models.py
@@ -1,26 +1,18 @@
+from dataclasses import dataclass
 import datetime
 from typing import Optional
 
 
+@dataclass
 class Author:
     name: str
     url: str
 
-    def __init__(self, *, name: str, url: str) -> None:
-        self.name = name
-        self.url = url
 
-
+@dataclass
 class Entry:
-    author: Optional[str]
-    author_url: Optional[str] = None
     date: datetime.date
     title: Optional[str]
     url: Optional[str]
-
-    def __init__(self, *, author: Optional[str], date: datetime.date,
-                 title: Optional[str], url: Optional[str]) -> None:
-        self.author = author
-        self.date = date
-        self.title = title
-        self.url = url
+    author: Optional[str]
+    author_url: Optional[str] = None
