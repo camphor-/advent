bcbec28867a8dd3dc77d803b8db58ffacb6ffc3d
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2020-11-29 11:49:57 +0900

    Refactor code to load YAML data files
    - Move data loading code to a separate module
    - Add tests for the new module
    - Install necessary packages for testing
    - Run tests on CI

--- advent/__main__.py
+++ advent/__main__.py
@@ -1,14 +1,10 @@
-from datetime import datetime
-from operator import attrgetter
 from pathlib import Path
 import sys
-from typing import Any, Dict, Iterable, List
+from typing import Any, Dict
 
 from jinja2 import Environment, FileSystemLoader
-import pytz
-import yaml
 
-from .models import Author, Entry
+from advent import data
 
 
 NUMBER_OF_LATEST_PUBLISHED_ENTRIES = 10
@@ -19,42 +15,6 @@ source_dir = root_dir / "source"
 output_dir = root_dir / "output"
 loader = FileSystemLoader(str(source_dir), encoding="utf-8")
 env = Environment(loader=loader)
-
-today = datetime.now(pytz.timezone("Asia/Tokyo")).date()
-
-
-def load_authors() -> Dict[str, Author]:
-    with open(data_dir / "authors.yml") as f:
-        authors_list = yaml.load(f, Loader=yaml.SafeLoader)
-    authors = [Author(**d) for d in authors_list]
-    return {author.name: author for author in authors}
-
-
-def load_entries() -> List[Entry]:
-    authors = load_authors()
-
-    def load_entry(d: Dict[str, Any]) -> Entry:
-        entry = Entry(**d)
-
-        if entry.author is not None:
-            entry.author_url = authors[entry.author].url
-
-        if entry.date > today:
-            # Not ready
-            entry.url = None
-
-        return entry
-
-    with open(data_dir / "entries.yml") as f:
-        return list(map(load_entry, yaml.load(f, Loader=yaml.SafeLoader)))
-
-
-def group_entries_by_year(entries: Iterable[Entry]) -> Dict[int, List[Entry]]:
-    entries_by_year = {}
-    years = list({e.date.year for e in entries})
-    for year in sorted(years, reverse=True):
-        entries_by_year[year] = [e for e in entries if e.date.year == year]
-    return entries_by_year
 
 
 def minify_html(html: str) -> str:
@@ -76,11 +36,13 @@ def render_and_write(
 
 
 def run(debug: bool = False) -> None:
-    entries = load_entries()
-    entries_by_year = group_entries_by_year(entries)
-    latest_published_entries = sorted(
-        filter(lambda e: e.url, entries), reverse=True, key=attrgetter("date")
-    )[:NUMBER_OF_LATEST_PUBLISHED_ENTRIES]
+    authors = data.load_authors(data_dir / "authors.yml")
+    entries = data.load_entries(data_dir / "entries.yml", authors)
+    entries_by_year = data.group_entries_by_year(entries)
+    latest_published_entries = data.get_last_published_entries(
+        entries,
+        NUMBER_OF_LATEST_PUBLISHED_ENTRIES,
+    )
     context = {
         "debug": debug,
         "description": (
---
+++ tests/test_data.py
@@ -0,0 +1,154 @@
+from datetime import date
+from pathlib import Path
+
+import pytest
+from pytest_mock import MockerFixture
+
+from advent.data import (
+    get_last_published_entries, group_entries_by_year,
+    load_authors, load_entries,
+)
+from advent.models import Author, Entry
+
+VALID_AUTHORS_YAML = """
+- name: camphor
+  url: https://example.com/camphor/
+- name: house
+  url: https://example.com/~house
+"""
+INVALID_AUTHORS_YAML = """
+- name: no-url
+"""
+AUTHORS = {
+    "camphor": Author(name="camphor", url="https://example.com/camphor/"),
+    "house": Author(name="house", url="https://example.com/~house"),
+}
+VALID_ENTRIES_YAML = """
+# Published
+- date: 2014-12-01
+  author: camphor
+  title: CAMPHOR- アドベントカレンダー 2014 を始めます!
+  url: https://example.com/news/advent-calendar-2014/
+
+# To be published
+- date: 2014-12-02
+  author: house
+  title: CAMPHOR- HOUSE
+  url: https://example.com/news/house/
+
+# Title/URL to be decided
+- date: 2015-12-01
+  author: camphor
+  title: null
+  url: null
+
+# Author to be decided
+- date: 2015-12-02
+  author: null
+  title: null
+  url: null
+"""
+INVALID_ENTRIES_YAML = """
+- date: today
+"""
+ENTRIES = [
+    Entry(
+        date=date(2014, 12, 1),
+        title="CAMPHOR- アドベントカレンダー 2014 を始めます!",
+        url="https://example.com/news/advent-calendar-2014/",
+        author="camphor",
+        author_url="https://example.com/camphor/",
+    ),
+    Entry(
+        date=date(2014, 12, 2),
+        title="CAMPHOR- HOUSE",
+        url=None,
+        author="house",
+        author_url="https://example.com/~house",
+    ),
+    Entry(
+        date=date(2015, 12, 1),
+        title=None,
+        url=None,
+        author="camphor",
+        author_url="https://example.com/camphor/",
+    ),
+    Entry(
+        date=date(2015, 12, 2),
+        title=None,
+        url=None,
+        author=None,
+        author_url=None,
+    ),
+]
+
+
+def test_load_authors_succeeds(mocker: MockerFixture) -> None:
+    mocker.patch("advent.data.open",
+                 mocker.mock_open(read_data=VALID_AUTHORS_YAML))
+    assert load_authors(Path("authors.yml")) == AUTHORS
+
+
+def test_load_authors_fails(mocker: MockerFixture) -> None:
+    mocker.patch("advent.data.open",
+                 mocker.mock_open(read_data=INVALID_AUTHORS_YAML))
+    with pytest.raises(TypeError):
+        load_authors(Path("authors.yml"))
+
+
+@pytest.mark.freeze_time("2014-12-01T12:00:00Z")
+def test_load_entries_succeeds(mocker: MockerFixture) -> None:
+    mocker.patch("advent.data.open",
+                 mocker.mock_open(read_data=VALID_ENTRIES_YAML))
+    assert load_entries(Path("entries.yml"), AUTHORS) == ENTRIES
+
+
+def test_load_entries_fails(mocker: MockerFixture) -> None:
+    mocker.patch("advent.data.open",
+                 mocker.mock_open(read_data=INVALID_ENTRIES_YAML))
+    with pytest.raises(TypeError):
+        load_entries(Path("entries.yml"), {})
+
+
+def test_group_entries_by_year() -> None:
+    # Verify insertion order as well
+    assert list(group_entries_by_year(ENTRIES).items()) == [
+        (2015, [ENTRIES[2], ENTRIES[3]]),
+        (2014, [ENTRIES[0], ENTRIES[1]]),
+    ]
+
+
+def test_get_last_published_entries() -> None:
+    entries = [
+        Entry(
+            date=date(2014, 12, i),
+            title=f"Entry #{i}",
+            url=f"https://example.com/news/entry-{i}",
+            author="camphor",
+            author_url="https://example.com/camphor/",
+        ) for i in range(1, 4)
+    ] + [
+        Entry(
+            date=date(2014, 12, i),
+            title=f"Entry #{i}",
+            url=None,
+            author="camphor",
+            author_url="https://example.com/camphor/",
+        ) for i in range(4, 7)
+    ]
+    assert get_last_published_entries(entries, 2) == [
+        Entry(
+            date=date(2014, 12, 3),
+            title="Entry #3",
+            url="https://example.com/news/entry-3",
+            author="camphor",
+            author_url="https://example.com/camphor/",
+        ),
+        Entry(
+            date=date(2014, 12, 2),
+            title="Entry #2",
+            url="https://example.com/news/entry-2",
+            author="camphor",
+            author_url="https://example.com/camphor/",
+        ),
+    ]
